2c2a1b03c13b75daadb696c969bf12e895d1874b
Zatinmohan <37806553+Zatinmohan@users.noreply.github.com>
2018-10-21 02:44:06 +0530

    Create bruteforce.cpp

---
+++ bruteforce.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int size;
+  
+  cin>>size;
+  
+  int arr[size];
+  
+  for(int i=0;i<size;i++)
+  {
+    cin>>arr[i]; 
+  }
+  
+  int count;
+  
+  for(int i=0;i<size;i++)
+  {
+    count=0;
+    for(int j=i+1;j<size;j++)
+    {
+      if(arr[i]==arr[j])
+      {
+        count++;
+      }
+    }
+    
+    if(count>size/2)
+    {
+      cout<<arr[i];
+      break;
+    }
+  }
+  
+  
+  return 0; 
+}
